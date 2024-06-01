ab3f6b855d5e36e1e878ef76d29d272dfbcd3c72
Michael-Burka <contact@michaelburka.com>
2024-06-01 20:44:31 +0400

    Add Opinion Form and Views for Adding and Displaying Opinions
    - Modified `opinions_app.py` to include additional imports: `redirect`, `render_template`, `url_for` from Flask, and `FlaskForm`, `StringField`, `SubmitField`, `TextAreaField`, `URLField` from Flask-WTF.
    - Added configuration for `SECRET_KEY`.
    - Created `OpinionForm` class with fields for title, text, source, and a submit button.
    - Updated `index_view` to render `opinion.html` template instead of returning plain text.
    - Added `add_opinion_view` to handle GET and POST requests for adding new opinions using `OpinionForm`.
    - Added `opinion_view` to display individual opinions based on their ID.
    - Updated routes to use corresponding templates in `templates/` directory.

--- opinions_app.py
+++ opinions_app.py
@@ -1,12 +1,16 @@
 from datetime import datetime
 from random import randrange
 
-from flask import Flask
+from flask import Flask, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, URLField
+from wtforms.validators import DataRequired, Length, Optional
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SECRET_KEY'] = 'Acrub'
 db = SQLAlchemy(app)
 
 
@@ -18,6 +22,23 @@ class Opinion(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 
+class OpinionForm(FlaskForm):
+    title = StringField(
+        'Введите название фильма',
+        validators=[DataRequired(message='Обязательное поле'),
+                    Length(1, 128)],
+    )
+    text = TextAreaField(
+        'Напишите мнение',
+        validators=[DataRequired(message='Обязательное поле')]
+    )
+    source = URLField(
+        'Добавьте ссылку на подробный обзор фильма',
+        validators=[Length(1, 256), Optional()]
+    )
+    submit = SubmitField('Добавить')
+
+
 @app.route('/')
 def index_view():
     quantity = Opinion.query.count()
@@ -27,7 +48,32 @@ def index_view():
 
     offset_value = randrange(quantity)
     opinion = Opinion.query.offset(offset_value).first()
-    return opinion.text
+
+    return render_template('opinion.html', opinion=opinion)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_opinion_view():
+    form = OpinionForm()
+
+    if form.validate_on_submit():
+        opinion = Opinion(
+            title=form.title.data,
+            text=form.text.data,
+            source=form.source.data
+
+        )
+        db.session.add(opinion)
+        db.session.commit()
+        return redirect(url_for('opinion_view', id=opinion.id))
+
+    return render_template('add_opinion.html', form=form)
+
+
+@app.route('/opinions/<int:id>')
+def opinion_view(id):
+    opinion = Opinion.query.get_or_404(id)
+    return render_template('opinion.html', opinion=opinion)
 
 
 if __name__ == '__main__':
